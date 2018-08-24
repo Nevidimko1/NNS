764b22bd1397160ed0f71707d3e9e46ab73ee549
Sergey Mankevich <s.mankevics@gmail.com>
2018-08-24 12:30:50 +0300

    development process updates

--- configs/generateDevLauncher.js
+++ configs/generateDevLauncher.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
+
 const scriptConfig = require('./scriptConfig')(true);
+const webpackDevConfig = require('../webpack.dev.config');
 
 const content = scriptConfig + `
 
 let script = document.createElement("script");
-script.src = 'http://localhost:8080/nns.user.js';
+script.src = 'http://localhost:${webpackDevConfig.devServer.port}/nns.user.js';
 script.type = 'text/javascript';
 document.documentElement.appendChild(script);
 `;
 
 console.log('Writing nns.dev.user.js script');
 try {
-    fs.writeFileSync('./dist/nns.dev.user.js', content);
+    if (!fs.existsSync('./.tmp')){
+        fs.mkdirSync('./.tmp');
+    }
+
+    fs.writeFileSync('./.tmp/nns.dev.user.js', content);
     console.log('Done');
 } catch (e) {
     console.error("Cannot write file ", e);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
+
 const config = require('./webpack.config');
+
 config.mode = 'development';
 config.devtool = 'inline-source-map';
+config.devServer = {
+    contentBase: path.join(__dirname, '.tmp'),
+    port: 3000,
+    inline: false
+}
 
 module.exports = config;
